0bd980ff4efba1aa466550356b7834e57e8309ff
Daivd Huosãr <davidvonhauser@gmail.com>
2014-03-30 15:33:54 +1100

    leverage localStorage for caching blogposts

--- app/js/posts/list.js
+++ app/js/posts/list.js
@@ -5,36 +5,66 @@
 var React = require('react'),
     $     = require('jquery'),
     Item  = require('./item'),
+    Posts = require('./post_data'),
     List;
 
 List = React.createClass({
 
   getInitialState: function () {
-    return { posts: [] };
+
+    var _array = [],
+        cached_data = localStorage.getItem('haustraliaer_towerPosts');
+
+    if(cached_data) {
+      _array = JSON.parse(cached_data);
+    }
+
+    return { posts: _array };
   },
   
   componentWillMount: function () {
-    this.fetch_local_data();
-  },
-
-  fetch_local_data: function() {
     this.fetch_latest_posts();
   },
   
   fetch_latest_posts: function () {
     $.ajax({
-      url:       'http://api.tumblr.com/v2/blog/haustraliaer.tumblr.com/posts/text?api_key=CC7nUBprgWxMr9hA85r5uqmXikN9GcSwlrygvmFKVGdFjE7cPy&filter=text',
       dataType:  'jsonp',
       data:      { format: 'jsonp' },
+      url:       'http://api.tumblr.com/v2/blog/haustraliaer.tumblr.com/posts/text?api_key=CC7nUBprgWxMr9hA85r5uqmXikN9GcSwlrygvmFKVGdFjE7cPy&filter=text',
 
       success: function (result) {
-        this.setState({ posts: result.response.posts });
-        console.log(result.response.posts);
+
+        var local_posts = this.state.posts,
+            local_length = local_posts.length,
+            remote_posts = result.response.posts.reverse(),
+            temp_posts = local_posts;
+
+
+        $(remote_posts).each(function(index) {
+
+          if((index + 1) > local_length) {
+            var object = {
+              id: this.id,
+              body: this.body
+            };
+
+            local_posts.push(object);
+
+            console.log('pushed new object');
+          }
+        });
+
+        this.setState({ posts: local_posts });
+        localStorage.setItem('haustraliaer_towerPosts', JSON.stringify(local_posts));
+
+        // todo... remove deleted posts by diffing the ajax result with local data
+        
       }.bind(this),
 
       error: function () {
         console.log("nope - couldn't connect");
       }.bind(this)
+
     });
   },
 
