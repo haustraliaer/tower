fb90c5046d7326f5616583b617e8656771fd399d
Daivd Huosãr <davidvonhauser@gmail.com>
2014-04-11 11:09:28 +1000

    get tumblr data!

--- app/application.js
+++ app/application.js
@@ -1,5 +1,7 @@
 
-
 var list = require('./widgets/list/list');
 
 list(document.getElementById("app"));
+
+
+
--- app/widgets/list/list.js
+++ app/widgets/list/list.js
@@ -1,8 +1,16 @@
 
+var $ = require('jquery');
 var Ractive = require('ractify');
 var sampleData = require('./list-data');
 var fadeTransition = require('../../utilities/transitions/fader');
 Ractive.transitions.fader = fadeTransition;
+
+var _array = [],
+    cached_data = localStorage.getItem('haustraliaer_towerPosts');
+
+if(cached_data) {
+  _array = JSON.parse(cached_data);
+}
 
 module.exports = function(el){
   
@@ -12,15 +20,51 @@ module.exports = function(el){
     data: { 
       greeting: 'hello', 
       recipient: '...ractive',
-      posts: sampleData,
+      posts: _array,
       transitions: {
-          fader: fadeTransition
+        fader: fadeTransition
       }
+    }
+  });
+
+
+  $.ajax({
+    dataType:  'jsonp',
+    data:      { format: 'jsonp' },
+    url:       'http://api.tumblr.com/v2/blog/haustraliaer.tumblr.com/posts/text?api_key=CC7nUBprgWxMr9hA85r5uqmXikN9GcSwlrygvmFKVGdFjE7cPy&filter=text',
+
+    success: function (result) {
+
+      var local_posts = ractive.get('posts'),
+          local_length = local_posts.length,
+          remote_posts = result.response.posts.reverse(),
+          temp_posts = local_posts;
+
+      $(remote_posts).each(function(index) {
+
+        if((index + 1) > local_length) {
+          var object = {
+            id: this.id,
+            body: this.body
+          };
+
+          local_posts.push(object);
+
+          console.log('pushed new object');
+        }
+      });
+
+      ractive.set('posts', local_posts);
+      localStorage.setItem('haustraliaer_towerPosts', JSON.stringify(local_posts));
+
+      // todo... remove deleted posts by diffing the ajax result with local data
+      
+    },
+
+    error: function () {
+      console.log("nope - couldn't connect");
     }
   });
 
   return ractive;
 }
-
-
-
