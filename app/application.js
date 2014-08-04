a8f9f3aaa67467c634056732ac072d3ef5ce60d9
haustraliaer <davidvonhauser@gmail.com>
2014-08-04 18:58:36 +1000

    Install react(ify)

--- app/application.js
+++ app/application.js
@@ -1,3 +1,18 @@
+/**
+ * @jsx React.DOM
+ */
+
 'use strict';
 
-console.log('dot')
+var React = require('react')
+
+var App = React.createClass({
+  render: function() {
+    return <div>Dot, {this.props.name}!</div>
+  }
+})
+
+React.renderComponent(
+  <App name="ditto" />,
+  document.getElementById('app')
+)
