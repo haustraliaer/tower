2d8ff4bfd42efc3d7a867bcf0fa553411911aef0
haustraliaer <davidvonhauser@gmail.com>
2014-09-09 23:30:26 +1000

    Update velocity

--- app/application.js
+++ app/application.js
@@ -6,11 +6,6 @@
 
 var React = window.React = require('react')
 var Tower = require('app-tower')
-var Velocity = require('velocity-animate');
-
-Velocity(document.body, {opacity: 0.5}, 2000).then(function(){
-  console.log('resolved');
-});
 
 
 // maybe put a loader here..
